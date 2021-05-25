f5ba23a6d37f5c174571a6f40301402cd0d60e3f
AllesonFernandes <allesonfernandes@gmail.com>
2021-05-25 17:58:44 -0300

    primeira tentativa

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+var nome="Alleson Fernandes";
+var idade = 22;
+var idade2 = 10;
+var frase = "A coreia do sul investe muito dinheiro em cultura";
+/*alert (nome + " tem " + idade + " anos");*/
+/*alert(idade+idade2)*/
+console.log(nome);
+console.log(idade+idade2);
+console.log(frase.toUpperCase());
+//alert(frase.replace("A coreia do sul", "O Brasil"))
